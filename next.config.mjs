8df171b8b6445fe0a373bf683f2e0c446a70cddd
Jonny Ortiz <114623944+MrJonnyOrtiz@users.noreply.github.com>
2026-01-02 10:31:38 -0500

    Initial commit: Spotlight template (static export ready)

---
+++ next.config.mjs
@@ -0,0 +1,27 @@
+import rehypePrism from '@mapbox/rehype-prism'
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  // outputFileTracingIncludes: {
+  //   '/articles/*': ['./src/app/articles/**/*.mdx'],
+  // },
+  // Enable static export to deploy to S3/CloudFront
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+}
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+})
+
+export default withMDX(nextConfig)
